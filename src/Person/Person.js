715cf191e98ae2f7005925a84daa4dbdde9f264b
Niyonsengaeric <niyeric11@gmail.com>
2019-12-16 09:52:04 +0200

    removing radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import './Person.css';
-import Radium from 'radium'
+
 
 const person = (props) => {
-  const style = {
-    '@media(min-width:500px)': {
-      width: '450px'
-    }
-
-  }
-
   return (
-    <div className='Person' style={style}>
+    <div className='Person'>
       <p onClick={props.click}>I am a {props.name} and i am {props.age} years old!</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
@@ -19,4 +12,4 @@ const person = (props) => {
   )
 };
 
-export default Radium(person);
+export default person;
