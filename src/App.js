5a9e9bc6a82111367f8cb40ea5e1a0fadde9effd
Niyonsengaeric <niyeric11@gmail.com>
2019-12-17 08:55:22 +0200

    Debugging handler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
 
 class App extends Component {
   state = {
@@ -51,13 +52,14 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
+            return <ErrorBoundary key={person.id}>
+              <Person
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id}
+
               changed={(event) => this.nameChangedHandler(event, person.id)}
-            />
+            /></ErrorBoundary>
           })}
 
         </div>
